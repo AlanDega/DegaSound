3f49d621f5aa8e84c4394f1c9eb91e11ffdcf7c9
Alan Dega <arkidega@gmail.com>
2019-09-20 13:57:53 -0500

    commit4

--- src/components/shared/Notification.vue
+++ src/components/shared/Notification.vue
@@ -1,18 +1,16 @@
 <template>
     <div>
-<v-container>
-    <v-row
-    align="center"
-    justify="center"
-    class="noti1"
-    >
-
-    
-    <v-alert color="blue" >
-        <slot name="body" > Algo anduvo mal  </slot>
-    </v-alert>
-    </v-row>
-</v-container>
+        <v-container>
+            <v-row
+            align="center"
+            justify="center"
+            class="noti1"
+            >
+            <v-alert color="blue" >
+                <slot name="body" > Algo anduvo mal  </slot>
+            </v-alert>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
